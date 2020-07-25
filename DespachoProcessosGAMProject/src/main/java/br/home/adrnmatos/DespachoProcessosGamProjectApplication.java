e2b527f21c1bb29d8343e7306aa37bcee55125b5
Adriano Matos <adrnmatos@gmail.com>
2020-07-25 18:57:25 -0400

    initial setup

---
+++ DespachoProcessosGAMProject/src/main/java/br/home/adrnmatos/DespachoProcessosGamProjectApplication.java
@@ -0,0 +1,13 @@
+package br.home.adrnmatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespachoProcessosGamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespachoProcessosGamProjectApplication.class, args);
+	}
+
+}
---
+++ DespachoProcessosGAMProject/src/test/java/br/home/adrnmatos/DespachoProcessosGamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.home.adrnmatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespachoProcessosGamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
